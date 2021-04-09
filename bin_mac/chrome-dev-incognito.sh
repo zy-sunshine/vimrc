89bc94bb753ebdc846d9b03c5240fbfdcb1b2c35
Zhang Yang <260328360@qq.com>
2021-04-09 13:01:51 +0800

    * add httpsServer and test cert

--- bin_mac/chrome-dev-incognito.sh
+++ bin_mac/chrome-dev-incognito.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-open -na Google\ Chrome --args --user-data-dir=/Users/netsec/tmp/chromeDevUserData --disable-web-security --disable-site-isolation-trials --incognito
+open -na Google\ Chrome --args --user-data-dir=/Users/netsec/tmp/chromeDevUserData --disable-web-security --disable-site-isolation-trials --ignore-certificate-errors --incognito
