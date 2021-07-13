68e8b599e45cac30006ab91feae2d71b36e6d631
Zhang Yang <260328360@qq.com>
2021-07-13 10:44:58 +0800

    * add taglist.vim

--- bin/httpsServer.py
+++ bin/httpsServer.py
@@ -1,10 +1,10 @@
 import http.server, ssl
 
-server_address = ('0.0.0.0', 4443)
+server_address = ('0.0.0.0', 8000)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
-                               certfile='/Users/netsec/bin/server_cert.pem',
-                               keyfile='/Users/netsec/bin/server_key.pem',
+                               certfile='/Users/netsec/.vim/bin/server_cert.pem',
+                               keyfile='/Users/netsec/.vim/bin/server_key.pem',
                                ssl_version=ssl.PROTOCOL_TLS)
 httpd.serve_forever()
--- bin_mac/chrome-dev.sh
+++ bin_mac/chrome-dev.sh
@@ -6,4 +6,4 @@ echo "
 # plugin ModHeader to set origin maunally.
 "
 
-open -na Google\ Chrome --args --user-data-dir=/Users/netsec/tmp/chromeDevUserData --disable-web-security --disable-site-isolation-trials --ignore-certificate-errors
+open -na Google\ Chrome --args --user-data-dir=/Users/netsec/tmp/chromeDevUserData --disable-web-security --disable-site-isolation-trials --ignore-certificate-errors --allow-running-insecure-content
